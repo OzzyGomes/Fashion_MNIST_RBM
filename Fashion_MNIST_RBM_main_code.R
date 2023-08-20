319ed0f7017bdf8db1c4b6f6a992c3bb058552de
OzzyGomes <ozzygomes@outlook.com>
2023-08-20 17:46:29 -0300

    first commit

---
+++ Fashion_MNIST_RBM_main_code.R
@@ -0,0 +1,18 @@
+library(dplyr)
+library(RBM)
+
+set.seed(0)
+
+data(Fashion)
+
+#'The labels are as follows: 
+#'0: T-shirt/tops 
+#'1: Trouser 
+#'2: Pullover 
+#'3: Dress 
+#'4: Coat 
+#'5: Sandal 
+#'6: Shirt 
+#'7: Sneaker 
+#'8: Bag 
+#'9: Ankle Boot 
